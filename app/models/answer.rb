33d508a55b1fe4a3fb3aea99f1428bd6dbf911d1
AyanoOkamuro <omochi185@gmail.com>
2021-09-20 09:00:56 +0000

    [add]nice function and view

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,9 +2,14 @@ class Answer < ApplicationRecord
 
   belongs_to :user
   belongs_to :question
+  has_many :nices, dependent: :destroy
 
   attachment :image
 
   validates :body, presence: true
+
+  def niced_by?(user)
+    nices.where(user_id: user.id).exists?
+  end
 
 end
