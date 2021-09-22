174021d436d10af1de503895c0d4a9d322a05870
AyanoOkamuro <omochi185@gmail.com>
2021-09-22 13:52:06 +0000

    [fix]view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admins do
-    get 'questions/index'
-    get 'questions/show'
-  end
-  namespace :admins do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :admins do
-    get 'users/index'
-    get 'users/edit'
-  end
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
   }
